bbd3f99692c5d86e74d590c1099131e296c6f6a5
nicolas <nmedda@viareport.com>
2013-11-21 15:48:33 +0100

    Suppressions des routes sur le seveur

--- app.js
+++ app.js
@@ -31,15 +31,6 @@ app.get('/', function (req, res) {
     res.sendfile('client/src/html/index.html');
 });
 
-app.get('/elements', function (req, res) {
-    res.json(elements);
-});
-
-app.post('/element', function (req, res) {
-    elements.push(req.body.value);
-    res.send();
-});
-
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log('Listening on http://localhost:'+ port);
