8ee1d96a5eb559ba1001912b209e5fa62ee86b7c
Christophe Promé <chprome@gmail.com>
2013-11-21 22:25:10 +0100

    4.1 : Ecriture des tests E2E

---
+++ client/test/E2E/dalek.js
@@ -0,0 +1,13 @@
+var baseUrl = 'http://localhost:5000/';
+
+module.exports = {
+    'Les éléments sont ajoutés à la liste' : function (test) {
+        test
+            .open(baseUrl)
+            .type('#ajouter-element-input', 'un élément')
+            .click('#ajouter-element-button')
+            .assert.numberOfElements('#elements-list li', 1, '1 élément présent dans la liste')
+            .assert.text('#elements-list li', 'un élément', 'Le text de l\'élément est correct')
+            .done();
+    }
+};
